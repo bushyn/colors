8c2a0fceacd0a226aa1ebb72432d5fcf8fdc85f0
Arseniy Bushyn <arseniybushyn@gmail.com>
2016-01-26 20:04:45 +0200

    Remove react

---
+++ src/script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+document.addEventListener('click', function (e) {
+  if (document.activeElement.tagName !== 'A') return;
+  getSelection().removeAllRanges();
+  var range = document.createRange();
+  range.selectNode(document.activeElement);
+  getSelection().addRange(range);
+  document.execCommand('copy') && getSelection().removeAllRanges();
+});
+
+document.addEventListener('mousedown', function (_ref) {
+  var t = _ref.target;
+  var x = _ref.offsetX;
+  var y = _ref.offsetY;
+
+  if (t.tagName == 'DIV') t.style.transformOrigin = x + 'px ' + y + 'px';
+});
---
+++ src/script.jsx
@@ -0,0 +1,13 @@
+document.addEventListener('click', e => {
+  if (document.activeElement.tagName !== 'A') return
+  getSelection().removeAllRanges()
+  let range = document.createRange()
+  range.selectNode(document.activeElement)
+  getSelection().addRange(range)
+  document.execCommand('copy') && getSelection().removeAllRanges()
+})
+
+document.addEventListener('mousedown', ({target:t, offsetX:x, offsetY:y}) => {
+  if (t.tagName == 'DIV') t.style.transformOrigin = `${x}px ${y}px`
+})
+
